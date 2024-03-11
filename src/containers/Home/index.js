630052412c0289a5d8ee96955a3737e8266c4c69
Foxcraftfury <138385017+sudopunk@users.noreply.github.com>
2024-03-11 17:21:22 +0530

    update gradient

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -81,7 +81,7 @@ class Home extends Component {
             <>
                 <NavBar home={true} />
                 <div className="home padding">
-                <div className="z-50 shadow-lg card bg-gradient-to-r from-423da1 via-20bdff to-A5FECB">
+                <div className="z-50 shadow-lg card bg-gradient-to-r from-#423da1 via-#20bdff to-#A5FECB">
                         <div className="left_content">
                             <h2>{variables[this.props.lang].welcome}</h2>
                             <p className="info">
